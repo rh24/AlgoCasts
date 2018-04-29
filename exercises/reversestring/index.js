027a454de3e3872bbb8f8f1237aebea94e96e131
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-04-29 15:02:30 -0400

    Done.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let val = "";
+  for(let i = str.length-1; i >= 0; i--) {
+    val += str[i];
+  }
+  return val;
+}
+
+// ES6 for...of:
+/*
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed += character;
+  }
+  return reversed;
+}
+*/
+
+// The easiest but not most obvious solution:
+// return str.split('').reverse().join('')
 
 module.exports = reverse;
