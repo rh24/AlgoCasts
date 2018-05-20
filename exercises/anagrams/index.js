b33372749c4dadf1b35e2652b197ace348e2b32a
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-20 15:06:05 -0400

    Add second anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,40 +9,52 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  let regStringA = stringA.replace(/[^\w]/g, "").toLowerCase();
-  let regStringB = stringB.replace(/[^\w]/g, "").toLowerCase();
-
-  let charMapA = {};
-  let charMapB = {};
-  let answer = false;
-
-  if (regStringA.length === regStringB.length) {
-    for (let char of regStringA) {
-      if (!Object.keys(charMapA).includes(char)) {
-        charMapA[char] = 1;
-      } else {
-        charMapA[char] += 1;
-      }
-    }
-
-    for (let char of regStringB) {
-      if (!Object.keys(charMapB).includes(char)) {
-        charMapB[char] = 1;
-      } else {
-        charMapB[char] += 1;
-      }
-    }
-
-    for (let char in charMapA) {
-      if (charMapA[char] === charMapB[char]) {
-        answer = true;
-      } else {
-        answer = false;
-      }
-    };
-  }
-
-  return answer;
+  return cleanString(stringA) === cleanString(stringB);
 }
 
+function cleanString(str) {
+  return str.replace(/[^\w/]/g, '').toLowerCase().split('').sort().join('');
+}
+
+// function anagrams(stringA, stringB) {
+//   let regStringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//   let regStringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+//
+//   let charMapA = {};
+//   let charMapB = {};
+//   let answer = false;
+//
+//   if (regStringA.length === regStringB.length) {
+//     for (let char of regStringA) {
+//       if (!Object.keys(charMapA).includes(char)) {
+//         charMapA[char] = 1;
+//       } else {
+//         charMapA[char] += 1;
+//       }
+//     }
+//
+//     for (let char of regStringB) {
+//       if (!Object.keys(charMapB).includes(char)) {
+//         charMapB[char] = 1;
+//       } else {
+//         charMapB[char] += 1;
+//       }
+//     }
+//
+//     for (let char in charMapA) {
+//       if (charMapA[char] === charMapB[char]) {
+//         answer = true;
+//       } else {
+//         answer = false;
+//       }
+//     };
+//   }
+//
+//   return answer;
+// }
+
 module.exports = anagrams;
+// below is helper function for building charMap
+// function buildCharMap(str) {
+//
+// }
