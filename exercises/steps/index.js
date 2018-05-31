6b06dacfe7cb043fdf28da01e9f16465f0daf795
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-31 15:05:30 -0400

    Add iterative and recursive solutions to steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,18 +17,57 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  let str = '';
-  let hash = '#';
-  let space = ' ';
+// function steps(n) {
+  // my first solution:
+  // let str = '';
+  // let hash = '#';
+  // let space = ' ';
+  //
+  // for (let i = 1; i <= n; i++) {
+  //   str += hash.repeat(i) // i number of times
+  //   space = space.repeat(n-i);
+  //   str += space;
+  //   console.log(str);
+  //   space = ' ';
+  //   str = '';
+  // }
+// }
 
-  for (let i = 1; i <= n; i++) {
-    str += hash.repeat(i) // i number of times
-    space = space.repeat(n-i);
-    str += space;
-    console.log(str);
-    space = ' ';
-    str = '';
+// two for loops solution (iterative solution):
+// do it iteratively and turn the for loop start/stop/iteration into recursive solutions;
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair);
+//   }
+// }
+
+recursive solution:
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1); // or:
+    // return;
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
 }
 module.exports = steps;
